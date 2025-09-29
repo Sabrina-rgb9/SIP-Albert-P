4bb48929708e2dde9c47bbda8a07d6dd48693866
Sabrina-rgb9 <selmeftah3.cf@iesesteveterradas.cat>
2025-09-29 15:54:48 +0200

    .

--- SIP-Albert/00 Java - Processos/Exemple 0002B/src/main/java/com/project/TaskStrategy.java
+++
@@ -1,7 +0,0 @@
-package com.project;
-
-import java.util.concurrent.ConcurrentHashMap;
-
-public interface TaskStrategy {
-    void execute(ConcurrentHashMap<String, Integer> sharedData) throws InterruptedException;
-}
